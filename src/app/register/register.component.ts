708a1d44b1f0ddccd9fd077af70f23010b2fd729
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-26 17:37:33 +0300

    updated code

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor( private fb: FormBuilder,private router:Router){}
+
+  registerForm!:FormGroup
+  ngOnInit(): void {
+    this.registerForm= this.fb.group({
+      name:['', Validators.required],
+      email:['', Validators.required, Validators.email],
+      password:['', Validators.required],
+      confirm_password:['', Validators.required]  
+    })
+  }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Injectable({
   providedIn: 'root'
